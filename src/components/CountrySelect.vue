4ea6d717547722bfc08149f440aebee4d105524f
ez <ezzatmakar93@gmail.com>
2021-04-26 01:06:22 +0200

    select edit

--- src/components/CountrySelect.vue
+++ src/components/CountrySelect.vue
@@ -1,26 +1,33 @@
 <template>
-  <select v-model="selected" class="text-center rounded w-full p-2">
-    <option value="0" class="form-select mt-10 block w-full border p-3 rounded">
-      Select Country
-    </option>
-    <option v-for="country in countries" :value="country.ID">
-      {{country.Country}}
-    </option>
-  </select>
+  <div class="text-center">
+    <select
+      v-model="selected"
+      class="shadow-md bg-blue-100 text-gray-700 border-none inline-block py-3 pl-3 pr-8 rounded leading-tight"
+    >
+      <option
+        value="0"
+        class="form-select mt-10 block w-full border p-3 rounded"
+      >
+        Select Country
+      </option>
+      <option v-for="country in countries" :key="country.ID">
+        {{ country.Country }}
+      </option>
+    </select>
+  </div>
 </template>
 
 <script>
 export default {
   name: "CountrySelect",
-  props: ['countries'],
+  props: ["countries"],
   data() {
     return {
-      selected: 0
-    }
-  }
-}
+      selected: 0,
+    };
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
